3b7ab37ed1c9b193a52f260dc798e87c62226c39
tacobayle <nicolas.nayle@gmail.com>
2025-10-04 07:31:03 +0200

    20251004-vcsa.sh

--- vcenter/vcsa.sh
+++ vcenter/vcsa.sh
@@ -6,14 +6,18 @@ rm -f ${resultFile}
 source /home/ubuntu/bash/variables.sh
 source /home/ubuntu/bash/log_message.sh
 source /home/ubuntu/bash/govc/load_govc_nested_env_wo_cluster.sh
-#
 load_govc_nested_env_wo_cluster
+#
+# port group
+#
 log_message "create portgroup ${basename_sddc}-pg-edge-overlay in vds ${basename_sddc}-vds-01 with vlan $(jq -c -r --arg arg "EDGE_OVERLAY" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "" "" ""
-govc dvs.portgroup.add -dvs "${basename_sddc}-vds-01" -vlan "$(jq -c -r --arg arg "EDGE_OVERLAY" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "${basename_sddc}-pg-edge-overlay" > /dev/null
+govc dvs.portgroup.add -dvs "${basename_sddc}-vds-01" -vlan "$(jq -c -r --arg arg "EDGE_OVERLAY" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "${basename_sddc}-pg-edge-overlay" > /dev/null 2>&1
 log_message "create portgroup ${basename_sddc}-pg-external in vds ${basename_sddc}-vds-01 with vlan $(jq -c -r --arg arg "EXTERNAL" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "" "" ""
-govc dvs.portgroup.add -dvs "${basename_sddc}-vds-01" -vlan "$(jq -c -r --arg arg "EXTERNAL" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "${basename_sddc}-pg-external" > /dev/null
-log_message "portgroups creation done" "" "${slack_webhook}" "${google_webhook}"
-touch ${resultFile}
+govc dvs.portgroup.add -dvs "${basename_sddc}-vds-01" -vlan "$(jq -c -r --arg arg "EXTERNAL" '.sddc.vcenter.networks[] | select( .type == $arg).vlan_id' $jsonFile)" "${basename_sddc}-pg-external" > /dev/null 2>&1
+#
+# content library
+#
 log_message "create content library update-cl-ubuntu" "" "" ""
-govc library.create ${vsphere_cl_name}
-govc library.import ${vsphere_cl_name} "/home/ubuntu/bin/$(basename ${ubuntu_ova_url})"
+govc library.create ${vsphere_cl_name} > /dev/null 2>&1
+govc library.import ${vsphere_cl_name} "/home/ubuntu/bin/$(basename ${ubuntu_ova_url})" > /dev/null 2>&1
+touch ${resultFile}
