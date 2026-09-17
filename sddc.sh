bd1c252cb3680e8c7e5950d51dcd85948dc27f7b
tacobayle <nicolas.nayle@gmail.com>
2026-09-17 12:28:16 +0200

    20260917-blueprints

--- sddc.sh
+++ sddc.sh
@@ -214,7 +214,7 @@ if [[ ${operation} == "apply" ]] ; then
           sed -e "s@\${avi_subdomain}@${avi_subdomain}@" \
               -e "s/\${domain}/${domain}/" /nested-vcf/templates/blueprint-gw-cert-manager.yaml.template | tee "/nested-vcf/vcf-automation/blueprint-gw-cert-manager.yaml" > /dev/null
           sed -e "s@\${avi_subdomain}@${avi_subdomain}@" \
-              -e "s/\${domain}/${domain}/" /nested-vcf/templates/blueprint-create-vs.yaml.template | tee "/nested-vcf/vcf-automation/blueprint-create-vs.yaml" > /dev/null
+              -e "s/\${domain}/${domain}/" /nested-vcf/templates/blueprint-create-vs.yaml.template | tee "/nested-vcf/vcf-automation/blueprints/blueprint-create-vs.yaml" > /dev/null
           echo $folders_to_copy | jq -c -r .[] | while read folder
           do
             scp -o StrictHostKeyChecking=no -r /nested-vcf/${folder} ubuntu@${ip_gw}:/home/ubuntu
